30c5cac3f5892cb993f5c581636f107686a94e8d
owner <owner@owners-MacBook-Pro.local>
2022-02-26 00:08:41 +0100

    update on the script

--- public/index.js
+++ public/index.js
@@ -1,24 +1,51 @@
-function calculateProfit() {
-  const pricePerBag = document.getElementById("pricePerBag").value;
-  const walletAmount = document.getElementById("walletAmount").value;
-  const profitPerBag = document.getElementById("profitPerBag").value;
-  const ownersPercentage = 0.3;
-  const customersPercentage = 0.7;
-
-  const totalBagsBought =
-    Math.floor(parseInt(walletAmount)) / parseInt(pricePerBag);
-  const totalProfit = totalBagsBought * parseInt(profitPerBag);
-  const ownersProfit = ownersPercentage * totalProfit;
-  const customersProfit = customersPercentage * totalProfit;
-
-  console.log({ ownersProfit, customersProfit });
-
-  document.getElementById("profit").innerHTML = `&#8358; ${customersProfit}`;
-}
-
-function reset() {}
-
 (() => {
   document.getElementById("ownersPercentage").value = '30%';
   document.getElementById("customersPercentage").value = '70%';
 })();
+
+let errors;
+let inputs;
+const isNumber = (value) => !isNaN(parseInt(value));
+
+function prepareElements() {
+  inputs = {
+    pricePerBag: document.getElementById("pricePerBag"),
+    walletAmount: document.getElementById("walletAmount"),
+    profitPerBag: document.getElementById("profitPerBag"),
+  };
+  errors = {
+    pricePerBagError: document.getElementById("pricePerBagError"),
+    walletAmountError: document.getElementById("walletAmountError"),
+    profitPerBagError: document.getElementById("profitPerBagError"),
+  }
+};
+
+function validateField(value, element) {
+  const requiredText = "This field is required";
+  const shouldBeANumberText = "Please input a number";
+  if(!value) element.innerHTML = requiredText;
+  if(!isNumber(value)) element.innerHTML = shouldBeANumberText;
+}
+
+function doValidation() { 
+  Object.values(inputs).forEach(({value}, index) => validateField(value, Object.values(errors)[index]));
+}
+
+function calculateProfit() {
+  prepareElements();
+  doValidation();
+  const ownersPercentage = 0.3;
+  const customersPercentage = 0.7;
+  const totalBagsBought = Math.floor(parseInt(inputs.walletAmount.value)) / parseInt(inputs.pricePerBag.value);
+  const totalProfit = totalBagsBought * parseInt(inputs.profitPerBag.value);
+  const ownersProfit = ownersPercentage * totalProfit;
+  const customersProfit = customersPercentage * totalProfit;
+  if(isNaN(customersProfit)) return;
+  document.getElementById("profit").innerHTML = `&#8358; ${customersProfit}`;
+}
+
+function reset() {
+  console.log({inputs, errors});
+  Object.values(inputs).forEach(input => input.value = "");
+  Object.values(errors).forEach(input => input.innerHTML = "");
+}
