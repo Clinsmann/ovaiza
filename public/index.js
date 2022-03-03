59cc1d3c264247f8a81f29d69dc654e7d8c4177a
owner <owner@owners-MacBook-Pro.local>
2022-03-03 13:13:30 +0100

    update on the styling, number forming, with love from tag

--- public/index.js
+++ public/index.js
@@ -1,12 +1,10 @@
-(() => {
-  document.getElementById("customersPercentage").value = '70%';
-})();
-
 let errors;
 let inputs;
-const isNumber = (value) => !isNaN(parseInt(value));
+const requiredText = "This field is required";
+const shouldBeANumberText = "Please input a number";
 
-function prepareElements() {
+(() => {
+  document.getElementById("customersPercentage").value = "70%";
   inputs = {
     pricePerBag: document.getElementById("pricePerBag"),
     walletAmount: document.getElementById("walletAmount"),
@@ -16,33 +14,53 @@ function prepareElements() {
     pricePerBagError: document.getElementById("pricePerBagError"),
     walletAmountError: document.getElementById("walletAmountError"),
     profitPerBagError: document.getElementById("profitPerBagError"),
-  }
+  };
+})();
+
+const isNumber = (value) => !isNaN(parseInt(value));
+
+const formatNumber = (number) =>
+  `₦${number}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+const validateField = (val, element) => {
+  let value = cleanValue(val);
+  if (!value) element.innerHTML = requiredText;
+  if (!isNumber(value)) element.innerHTML = shouldBeANumberText;
 };
 
-function validateField(value, element) {
-  const requiredText = "This field is required";
-  const shouldBeANumberText = "Please input a number";
-  if(!value) element.innerHTML = requiredText;
-  if(!isNumber(value)) element.innerHTML = shouldBeANumberText;
-}
+const doValidation = () =>
+  Object.values(inputs).forEach(({ value }, index) =>
+    validateField(value, Object.values(errors)[index])
+  );
 
-function doValidation() { 
-  Object.values(inputs).forEach(({value}, index) => validateField(value, Object.values(errors)[index]));
-}
+const cleanValue = (val) => {
+  let value = val.replace(/ /g, "");
+  value = value.replace(/₦/g, "");
+  value = value.replace(/,/g, "");
+  return value;
+};
 
-function calculateProfit() {
-  prepareElements();
+const reset = () => {
+  Object.values(inputs).forEach((input) => (input.value = ""));
+  Object.values(errors).forEach((input) => (input.innerHTML = ""));
+};
+
+const calculateProfit = () => {
   doValidation();
   const customersPercentage = 0.7;
-  const totalBagsBought = Math.floor(parseInt(inputs.walletAmount.value)) / parseInt(inputs.pricePerBag.value);
-  const totalProfit = totalBagsBought * parseInt(inputs.profitPerBag.value);
+  const totalBagsBought =
+    Math.floor(parseInt(cleanValue(inputs.walletAmount.value))) /
+    parseInt(cleanValue(inputs.pricePerBag.value));
+  const totalProfit =
+    totalBagsBought * parseInt(cleanValue(inputs.profitPerBag.value));
   const customersProfit = customersPercentage * totalProfit;
-  if(isNaN(customersProfit)) return;
-  document.getElementById("profit").innerHTML = `&#8358; ${customersProfit}`;
-}
+  if (isNaN(customersProfit)) return;
+  document.getElementById("profit").innerHTML = formatNumber(customersProfit);
+};
 
-function reset() {
-  console.log({inputs, errors});
-  Object.values(inputs).forEach(input => input.value = "");
-  Object.values(errors).forEach(input => input.innerHTML = "");
-}
+Object.values(inputs).forEach((element) => {
+  element.addEventListener("keyup", function () {
+    let value = cleanValue(this.value);
+    if (isNumber(value)) this.value = formatNumber(value);
+  });
+});
